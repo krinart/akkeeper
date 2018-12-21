fdc9c7f71914f6ed988b7222d193701ff9b4ae04
Viktor Ershov <krinart@gmail.com>
2018-12-20 23:01:39 -0800

    More tiny cleanups

--- akkeeper/src/main/scala/akkeeper/master/service/RequestTrackingService.scala
+++ akkeeper/src/main/scala/akkeeper/master/service/RequestTrackingService.scala
@@ -28,11 +28,11 @@ private[service] trait RequestTrackingService extends Actor with ActorLogging {
   private var currentServiceReceive: Option[Receive] = None
 
   private def getServiceReceive: Receive = {
-    currentServiceReceive.getOrElse(serviceReceive)
+    currentServiceReceive getOrElse serviceReceive
   }
 
   private def baseReceive: Receive = {
-    case request: WithRequestId if (trackedMessages.contains(request.getClass)) =>
+    case request: WithRequestId if trackedMessages.contains(request.getClass) =>
       sendersById.put(request.requestId, SenderContext(sender(), None))
       getServiceReceive(request)
   }
